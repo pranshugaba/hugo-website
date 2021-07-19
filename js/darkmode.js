ca53ba4a1397107214d0e45b5e0113eb9d9d632e
pranshugaba <pranshugaba@users.noreply.github.com>
2021-07-19 16:23:40 +0530

    deploy: 2d33d3bba34cad5ac142ffda72751e14e249e494

---
+++ js/darkmode.js
@@ -0,0 +1,28 @@
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+  // add the class dark-mode to the body
+  document.body.classList.add('dark-mode');
+  // update darkMode in localStorage
+  localStorage.setItem('darkMode', 'enabled');
+};
+
+const disableDarkMode = () => {
+  // add the class dark-mode to the body
+  document.body.classList.remove('dark-mode');
+  // update darkMode in localStorage
+  localStorage.setItem('darkMode', 'disabled');
+};
+
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode');
+  if (darkMode !== 'disabled') {
+    disableDarkMode();
+  } else {
+    enableDarkMode();
+  }
+});
