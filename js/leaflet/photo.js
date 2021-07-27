dc8b80061c868e6bf3ac9de041832ba18b394b45
pranshugaba <pranshugaba@users.noreply.github.com>
2021-07-28 00:10:34 +0530

    deploy: f643c683740563a22f66ce9138120cbb7ee74b2f

--- js/leaflet/photo.js
+++ js/leaflet/photo.js
@@ -53,7 +53,7 @@ if (L.MarkerClusterGroup) {
   L.Photo.Cluster = L.MarkerClusterGroup.extend({
     options: {
       featureGroup: L.photo,
-      maxClusterRadius: 60,
+      maxClusterRadius: 75,
       showCoverageOnHover: false,
       iconCreateFunction: function (cluster) {
         return new L.DivIcon(
